6b7eaf53d4ee3b5b182b3d4ba19afaf16715b6bd
a11isonliu <allison.liu@colorado.edu>
2021-11-29 16:03:56 -0700

    troubleshooting test.py

--- train.py
+++ train.py
@@ -6,7 +6,24 @@ from models import create_model
 from util.visualizer import Visualizer
 import numpy as np
 
-torch.cuda.empty_cache()
+from GPUtil import showUtilization as gpu_usage
+from numba import cuda
+
+def free_gpu_cache():
+    print("Initial GPU Usage")
+    gpu_usage()                             
+
+    torch.cuda.empty_cache()
+
+    cuda.select_device(0)
+    cuda.close()
+    cuda.select_device(0)
+
+    print("GPU Usage after emptying the cache")
+    gpu_usage()
+
+free_gpu_cache()                           
+
 
 
 if __name__ == '__main__':
