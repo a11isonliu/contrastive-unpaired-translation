e7d610c83903bd6ec17a68639bc5957a34548e64
Allison Liu <allison.liu@colorado.edu>
2022-02-25 11:52:10 -0700

    commenting out free_gpu_cache in train.py

--- train.py
+++ train.py
@@ -9,20 +9,20 @@ import numpy as np
 from GPUtil import showUtilization as gpu_usage
 from numba import cuda
 
-def free_gpu_cache():
-    print("Initial GPU Usage")
-    gpu_usage()                             
+# def free_gpu_cache():
+#     print("Initial GPU Usage")
+#     gpu_usage()                             
 
-    torch.cuda.empty_cache()
+#     torch.cuda.empty_cache()
 
-    cuda.select_device(0)
-    cuda.close()
-    cuda.select_device(0)
+#     cuda.select_device(0)
+#     cuda.close()
+#     cuda.select_device(0)
 
-    print("GPU Usage after emptying the cache")
-    gpu_usage()
+#     print("GPU Usage after emptying the cache")
+#     gpu_usage()
 
-free_gpu_cache()                           
+# free_gpu_cache()                           
 
 
 
