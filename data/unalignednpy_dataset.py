5e79862916ee6936deee60d638db69f9442ce1a6
Allison Liu <allison.liu@colorado.edu>
2022-04-16 23:17:47 -0600

    edited unalignednpy_dataset to use .npy files

--- data/unalignednpy_dataset.py
+++ data/unalignednpy_dataset.py
@@ -58,14 +58,14 @@ class UnalignedNpyDataset(BaseDataset):
         #load npy array
         A_arr = np.load(A_path)
         B_arr = np.load(B_path)
-        A_img = Image.fromarray(A_arr) #open npy array
-        B_img = Image.fromarray(B_arr) #open npy array
+        # A_img = Image.fromarray(A_arr) #open npy array
+        # B_img = Image.fromarray(B_arr) #open npy array
     
         # A_img = Image.open(A_path).convert('RGB')
         # B_img = Image.open(B_path).convert('RGB')
         # apply image transformation
-        A = self.transform_A(A_img)
-        B = self.transform_B(B_img)
+        A = self.transform_A(A_arr)
+        B = self.transform_B(B_arr)
 
         return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
